305e57e9c65c71eabe217efcb838151ae59802a3
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-11-24 05:42:02 +0200

    Refectored the Code by adding utils file

---
+++ utils.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.showReviewTotal = showReviewTotal;
+exports.populateUser = populateUser;
+var reviewTotalDisplay = document.querySelector('#reviews');
+var returningUserDisplay = document.querySelector('#returning-user');
+var userNameDisplay = document.querySelector('#user');
+function showReviewTotal(value, reviewer, isLoyalty) {
+    var iconDisplay = isLoyalty ? '⭐' : '';
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay;
+}
+function populateUser(isReturning, userName) {
+    if (isReturning == true) {
+        returningUserDisplay.innerHTML = 'back';
+    }
+    userNameDisplay.innerHTML = userName;
+}

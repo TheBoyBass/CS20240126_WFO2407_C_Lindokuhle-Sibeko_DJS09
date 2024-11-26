a6663724f3accc987ac82ba20643e4da758cd58c
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-11-26 09:14:42 +0200

    "Moved Review and Property interfaces from index.ts to interfaces.ts, removed duplicate enum declarations, and removed unused imports."

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import { showReviewTotal, populateUser, showDetails, getTopTwoReviews} from './utils'
-import { Price, Country } from './types'
 import { Permissions , LoyaltyUser } from './enums'
-import  Review  from './interfaces'
+import { Review, Property } from './interfaces'
 const propertyContainer = document.querySelector('.properties')
 const reviewContainer = document.querySelector('.reviews')
 const container = document.querySelector('.container')
@@ -9,24 +8,6 @@ const button = document.querySelector('button')
 const footer = document.querySelector('.footer')
 
 let isLoggedIn: boolean
-
-// enum Permissions {
-//     ADMIN = 'ADMIN', 
-//     READ_ONLY = 'READ_ONLY'
-// }
-
-// enum LoyaltyUser {
-//     GOLD_USER = 'GOLD_USER',
-//     SILVER_USER = 'SILVER_USER',
-//     BRONZE_USER = 'BRONZE_USER'
-// }
-
-// interface Review {
-//     name: string; 
-//     stars: number; 
-//     loyaltyUser: LoyaltyUser; 
-//     date: string;   
-// }
 
 // Reviews
 const reviews: Review[] = [
@@ -57,20 +38,6 @@ const you = {
     isReturning: true,
     age: 35,
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
-}
-
-interface Property {
-    image: string;
-    title: string;
-    price: Price;
-    location: {
-        firstLine: string;
-        city: string;
-        code: number | string;
-        country: Country
-    }
-    contact: [ number, string];
-    isAvailable: boolean;
 }
 
 // Array of Properties
